611c37a99d59aee0ec4575bebb4372ef4dac7983
julianromera <julian.romera@gmail.com>
2014-02-14 05:00:24 +0100

    fixing fixes..

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -214,8 +214,8 @@ if ($user->uid) {
 -->
 	</div>
     
-    <a href="//europa.eu"><img src="//voa3r.eu/images/flag_eu.gif" border="0" style="border: 0; float: left;"></a>
-    <a href="//ec.europa.eu/research/fp7/"><img src="//voa3r.eu/images/fp7.gif" border="0" style="border: 0; float: left;"></a>
+    <a href="//europa.eu"><img src="/sites/all/themes/tweme/assets/images/flag_eu.gif" border="0" style="border: 0; float: left;"></a>
+    <a href="//ec.europa.eu/research/fp7/"><img src="/sites/all/themes/tweme/assets/images/fp7.gif" border="0" style="border: 0; float: left;"></a>
     <?php print "&nbsp;&nbsp;" . $copyright ?>
      <a href="/about" title="About us">About us</a><!-- | <a href="/project" class="">The Project</a>--> | <a href="/contact" class="">Contact</a> | <a href="/legal" class="colorbox-load">Legal Information</a>
       <div class="span10 marlef01">
